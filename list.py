8e666325334e3f5e398d4f2fb00ef4b70254f1b7
Aman <aman.sunny08@gmail.com>
2023-01-02 16:45:12 +0100

    mango updated

--- list.py
+++ list.py
@@ -3,7 +3,7 @@ x = list(range(1,11))
 print("##########Complete list###################")
 print(x)
 
-print("########## for loop###################")
+print("########## for loop################")
 for i in x:
     print(i)
 
@@ -15,6 +15,8 @@ else:
 
 print("############append list#################")
 
+x.append("orange")
+x.append("kiwi")
 x.append("apple")
 x.append("mango")
 print(x)
